09c485917c0df2c6bc006c05d29e174147e2da79
susiwen.ssw <susiwen.ssw@antgroup.com>
2022-01-14 18:53:35 +0800

    refac: icon

--- packages/widgets/src/Layout/AppIcon/index.tsx
+++ packages/widgets/src/Layout/AppIcon/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import * as antdIcon from '@ant-design/icons';
 import classNames from 'classnames';
 import styles from './index.less';
+import { BaseLayoutComp } from '../BaseLayoutComp';
 
 export interface IIcon {
   type: 'ICON' | 'ANTD' | 'IMG' | 'HTML';
@@ -23,37 +24,45 @@ export default function AppIcon({ config, className }: IProps) {
     const TargetIcon = antdIcon[content];
     if (TargetIcon) {
       return (
-        <TargetIcon
-          style={style}
-          className={classNames([className, styles.appIcon])}
-        />
+        <BaseLayoutComp type="antdIcon">
+          <TargetIcon
+            style={style}
+            className={classNames([className, styles.appIcon])}
+          />
+        </BaseLayoutComp>
       );
     }
   }
   if (type === 'ICON') {
     return (
-      <i
-        className={classNames([content, className, styles.appIcon])}
-        style={style}
-      />
+      <BaseLayoutComp type="icon">
+        <i
+          className={classNames([content, className, styles.appIcon])}
+          style={style}
+        />
+      </BaseLayoutComp>
     );
   }
   if (type === 'IMG') {
     return (
-      <img
-        src={content}
-        style={style}
-        className={classNames([className, styles.appIcon])}
-      />
+      <BaseLayoutComp type="img">
+        <img
+          src={content}
+          style={style}
+          className={classNames([className, styles.appIcon])}
+        />
+      </BaseLayoutComp>
     );
   }
   if (type === 'HTML') {
     return (
-      <span
-        dangerouslySetInnerHTML={{ __html: content }}
-        style={style}
-        className={classNames([className, styles.appIcon])}
-      />
+      <BaseLayoutComp type="html">
+        <span
+          dangerouslySetInnerHTML={{ __html: content }}
+          style={style}
+          className={classNames([className, styles.appIcon])}
+        />
+      </BaseLayoutComp>
     );
   }
   return <></>;
