781b275d9960e6a0d81577f9a9baf899e60e1f9d
susiwen.ssw <susiwen.ssw@antgroup.com>
2022-02-07 15:45:05 +0800

    feat: 筛选器

--- packages/widgets/src/Filter/Select.tsx
+++ packages/widgets/src/Filter/Select.tsx
@@ -1,19 +1,26 @@
 import { Select } from 'antd';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { IWidgetProps } from '@antv/dipper-core';
-import { useConfigService } from '@antv/dipper-layout';
+import { useWidgets } from '@antv/dipper-layout';
 
-export function DipperSelect({ type = 'dipperselect', options }: IWidgetProps) {
-  const { setWidgetsValue } = useConfigService();
+export function DipperSelect({ id, type = 'select', options }: IWidgetProps) {
+  const { widget } = useWidgets(id || type);
+  console.log(id);
+  useEffect(() => {
+    widget?.setValues(
+      options?.defaultValue || options?.options?.[0]?.value || '',
+    );
+  }, []);
+
   return (
     <Select
-      defaultValue={options?.defaultValue || ''}
+      defaultValue={options?.defaultValue || options?.options?.[0]?.value || ''}
       options={options?.options || []}
       bordered={false}
       style={{ width: 180 }}
       allowClear={false}
       onChange={(e: any) => {
-        setWidgetsValue(type, e);
+        widget?.setValues(e);
       }}
     />
   );
