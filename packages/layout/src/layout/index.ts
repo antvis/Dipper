4b603acc08b3276ab502829f0432ee8846d95836
susiwen.ssw <susiwen.ssw@antgroup.com>
2022-02-21 15:57:04 +0800

    feat: 移动端布局

--- packages/layout/src/layout/index.ts
+++ packages/layout/src/layout/index.ts
@@ -1,4 +1,4 @@
 export { default as DipperHeader, TitleDom, LogoDom } from './Header';
 export { default as DipperContainer } from './Container';
-export { default as DipperMobileContainer } from './mobile/Container/mobile';
+export { default as DipperMobileContainer } from './mobile';
 export * from './baseLayout';
