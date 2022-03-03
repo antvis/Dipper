e32dd17b65815aa17ff2c4fc0314a5d0e78a56fe
lzxue <lzx199065@gmail.com>
2022-03-03 17:17:03 +0800

    chore: 更新地图配置

--- packages/pc/src/layout/Container/index.tsx
+++ packages/pc/src/layout/Container/index.tsx
@@ -4,8 +4,9 @@ import styles from './index.less';
 import DipperHeader from '../Header';
 import ToolBar from '../Toolbar';
 import { MapContainer } from './MapContainer';
+import { IConfig } from '@antv/dipper-core';
 import { DipperContainerContext, IContainerProps } from '@antv/dipper-layout';
-export default function DipperContainer({ cfg, children, onLoad }: IContainerProps) {
+export default function DipperContainer({ cfg, children, onLoad }: IContainerProps<IConfig>) {
   return (
     <DipperContainerContext cfg={cfg} onLoad={onLoad}>
       <Layout className={styles.pageMap}>
