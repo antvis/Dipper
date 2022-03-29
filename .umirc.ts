3cf292d7df2e0d5c949de87e18d647e3280702a1
yunji <yunji.me@outlook.com>
2022-03-29 14:50:37 +0800

    chore: 正式环境测试文档分离

--- .umirc.ts
+++ .umirc.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'dumi';
 
 const logo = 'https://antv-2018.alipay.com/assets/image/icon/l7.svg';
+const isProduction = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   title: 'Dipper',
   favicon: logo,
   logo,
   mode: 'site',
+  outputPath: 'docs-dist',
+  publicPath: '/dipper/',
+  base: '/dipper/',
   navs: [
     null,
     {
@@ -14,10 +18,9 @@ export default defineConfig({
       path: 'https://github.com/antvis/Dipper',
     },
   ],
-  outputPath: 'docs-dist',
-  publicPath: '/Dipper/',
-  base: '/Dipper/',
-  headScripts: ["localStorage.setItem('dumi:prefers-color', 'light')"],
+  resolve: {
+    excludes: isProduction ? ['docs/dev'] : [],
+  },
   analytics: {
     // Google Analytics 代码，配置后会启用
     ga: 'G-CBX7JL1Q57',
