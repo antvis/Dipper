f34aa643aca3014fb716ce7d09aed3db522884ec
lvisei <yunji.me@outlook.com>
2022-06-08 15:25:56 +0800

    fix: 修复依赖注入容器复用的情况 (#158)

--- packages/layout/src/layout/Container/Context.tsx
+++ packages/layout/src/layout/Container/Context.tsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import type { Dipper, IBaseConfig } from '@antv/dipper-core';
 import { useUnmount } from 'ahooks';
-import { Provider } from 'inversify-react';
-import type { IBaseConfig } from '@antv/dipper-core';
-import type { Dipper } from '@antv/dipper-core';
-import { useDipperContainer } from '../../hooks';
 import type { Container } from 'inversify';
+import { Provider } from 'inversify-react';
+import React, { useEffect } from 'react';
+import { useDipperContainer } from '../../hooks';
 
 export interface IContainerProps<T = any> {
   /** 布局根容器，初始化化上下文 */
@@ -29,7 +28,10 @@ export function DipperContainerContext<T = any>({ cfg, children, onLoad }: ICont
   }, [sceneContainer]);
 
   return sceneContainer ? (
-    <Provider container={sceneContainer?.getContainer() as Container}>{children}</Provider>
+    // key for rendering: https://github.com/Kukkimonsuta/inversify-react/blob/master/src/provider.tsx#L50
+    <Provider key={Math.random()} container={sceneContainer?.getContainer() as Container}>
+      {children}
+    </Provider>
   ) : (
     <></>
   );
