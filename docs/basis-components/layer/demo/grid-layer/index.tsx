5ce058f2dd2356a791e2745811f154a1035a1fc8
lzxue <lzx199065@gmail.com>
2022-06-10 12:36:54 +0800

    chore: lint error

--- docs/basis-components/layer/demo/grid-layer/index.tsx
+++ docs/basis-components/layer/demo/grid-layer/index.tsx
@@ -4,13 +4,6 @@ import { config } from './config/config';
 import type { IConfig } from '@antv/dipper';
 import { DipperContainer } from '@antv/dipper';
 
-interface IInitData {
-  areaVOList: any[];
-  sceneCode: string;
-  areaCode: string;
-  filterData: any[];
-}
-
 export default function LayerDemo() {
   const [mapConfig, setMapConfig] = useState<IConfig>();
   // 初始化相关数据
